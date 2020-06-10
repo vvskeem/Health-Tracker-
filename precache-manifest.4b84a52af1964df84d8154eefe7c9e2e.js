b06c2fa7e6d8b6932bb5ef7a9b6e78726c813ef0
Shakeem Albouy <albouyshakeem@gmail.com>
2020-06-10 03:54:42 -0300

    Updates

---
+++ precache-manifest.4b84a52af1964df84d8154eefe7c9e2e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fd1d31b3a0c02f51c32cc6c1fa9b5353",
+    "url": "/react-gh-pages/index.html"
+  },
+  {
+    "revision": "a7e73f8c8ba8177420d0",
+    "url": "/react-gh-pages/static/css/main.f16de723.chunk.css"
+  },
+  {
+    "revision": "ec6df55df3371e3f0dbc",
+    "url": "/react-gh-pages/static/js/2.8a59a620.chunk.js"
+  },
+  {
+    "revision": "c6d01cb19213691bc81a274434d00fe7",
+    "url": "/react-gh-pages/static/js/2.8a59a620.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a7e73f8c8ba8177420d0",
+    "url": "/react-gh-pages/static/js/main.ec316caf.chunk.js"
+  },
+  {
+    "revision": "4ed38cfcc1900e6b2e19",
+    "url": "/react-gh-pages/static/js/runtime-main.46bc41ec.js"
+  }
+]);
